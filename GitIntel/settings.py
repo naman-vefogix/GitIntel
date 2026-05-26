a5fb5cc242b4a620092e1bb48f187192dcf45e24
Naman-vefogix <namankalwani.vefogix@gmail.com>
2026-05-26 11:42:17 +0530

    chore: migrate database from SQLite3 to PostgreSQL

--- GitIntel/settings.py
+++ GitIntel/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'GitIntel.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'GitIntel',
+        'USER': 'postgres',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
